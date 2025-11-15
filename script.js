2c96c057e64ac9a428f6b26cd28e41f02406212e
Kelvin Phang <kelvinphc98@gmail.com>
2025-11-15 17:55:07 +0800

    Make the calculator functional with caveats
    Calculator can operate but:
    - Clear, Delete and decimal still do not work
    - After doing at least 1 calculation, clicking numbers before clicking
    operator breaks the calculator

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const numbers = document.querySelectorAll(".number");
+const display = document.getElementById("display");
+const operators = document.querySelectorAll(".operator");
+
+let firstNumber = "";
+let currentOperator = "";
+let secondNumber = "";
+let previousOperator = "";
+
 function add(a, b) {
     firstNumber = a + b;
     display.textContent = firstNumber;
@@ -18,28 +27,8 @@ function divide(a, b) {
     display.textContent = firstNumber;
 };
 
-const numbers = document.querySelectorAll(".number");
-const display = document.getElementById("display");
-const operators = document.querySelectorAll(".operator");
-
-let firstNumber = "";
-let selectedOperator = "";
-let secondNumber = "";
-
-numbers.forEach((number) => {
-    number.addEventListener("click", () => {
-        if (selectedOperator == "") {
-            firstNumber += number.textContent;
-            display.textContent = firstNumber;
-        } else {
-            secondNumber += number.textContent;
-            display.textContent = secondNumber;
-        };
-    });
-});
-
-function operate(firstNumber, selectedOperator, secondNumber) {
-    switch(selectedOperator) {
+function operate(firstNumber, operator, secondNumber) {
+    switch(operator) {
         case "add":
             add(Number(firstNumber), Number(secondNumber));
             break;
@@ -55,12 +44,27 @@ function operate(firstNumber, selectedOperator, secondNumber) {
     };
 };
 
-operators.forEach((operator) => {
-    operator.addEventListener("click", () => {
-        selectedOperator = operator.id;
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        if (currentOperator == "") {
+            firstNumber += number.textContent;
+            display.textContent = firstNumber;
+        } else {
+            secondNumber += number.textContent;
+            display.textContent = secondNumber;
+        };
     });
 });
 
-document.addEventListener("keypress", () => {
-    operate(firstNumber, selectedOperator, secondNumber);
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        previousOperator = currentOperator;
+        currentOperator = operator.id;
+        if (secondNumber != "") {
+            operate(firstNumber, previousOperator, secondNumber);
+            secondNumber = "";
+        } else {
+            secondNumber = "";
+        };
+    });
 });
